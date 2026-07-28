97c7354e8b652887a197937e18877cad8b15daa6
Kent Davis <kentcharlesdavis@gmail.com>
2026-07-28 12:25:25 +1000

    made research logbook form with data binding

---
+++ WhaleResearchApp.Shared/Models/LogbookEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhaleResearchApp.Shared.Models;
+
+public class LogbookEntry
+{
+    public Guid LogbookEntryId { get; set; } = Guid.NewGuid();
+
+    public DateOnly TourDate { get; set; }
+
+    public TimeOnly TourTime { get; set; }
+
+    public string Skipper { get; set; } = "";
+
+    public string Researchers { get; set; } = "";
+
+    public string Sightings { get; set; } = "";
+
+    public string? PhotosReference { get; set; }
+
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.Now;
+
+    public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.Now;
+}

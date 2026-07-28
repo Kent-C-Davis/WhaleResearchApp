3bda5a4c55d538f533c32980fd271b1a8187ab00
Kent Davis <kentcharlesdavis@gmail.com>
2026-07-28 12:38:25 +1000

    Added validation to form

--- WhaleResearchApp.Shared/Models/LogbookEntry.cs
+++ WhaleResearchApp.Shared/Models/LogbookEntry.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace WhaleResearchApp.Shared.Models;
 
 public class LogbookEntry
 {
     public Guid LogbookEntryId { get; set; } = Guid.NewGuid();
 
+    [Required]
     public DateOnly TourDate { get; set; }
 
+    [Required]
     public TimeOnly TourTime { get; set; }
 
+    [Required]
     public string Skipper { get; set; } = "";
 
+    [Required]
     public string Researchers { get; set; } = "";
 
+    [Required]
     public string Sightings { get; set; } = "";
 
     public string? PhotosReference { get; set; }
